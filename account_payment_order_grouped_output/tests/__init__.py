f1822106b0a3dfd819eccfe68b5a6ae6470d0b4f
Randy La Rosa Alvarez <rra@wedoo.tech>
2024-04-05 05:13:08 -0400

    as

--- account_payment_order_grouped_output/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import test_payment_order_inbound_grouped
--- setup/_metapackage/setup.py
+++ setup/_metapackage/setup.py
@@ -9,18 +9,12 @@ setuptools.setup(
     version=version,
     install_requires=[
         'odoo-addon-account_banking_mandate>=15.0dev,<15.1dev',
-        'odoo-addon-account_banking_mandate_contact>=15.0dev,<15.1dev',
-        'odoo-addon-account_banking_mandate_sale>=15.0dev,<15.1dev',
         'odoo-addon-account_banking_pain_base>=15.0dev,<15.1dev',
         'odoo-addon-account_banking_sepa_credit_transfer>=15.0dev,<15.1dev',
         'odoo-addon-account_banking_sepa_direct_debit>=15.0dev,<15.1dev',
         'odoo-addon-account_payment_mode>=15.0dev,<15.1dev',
         'odoo-addon-account_payment_order>=15.0dev,<15.1dev',
-        'odoo-addon-account_payment_order_grouped_output>=15.0dev,<15.1dev',
-        'odoo-addon-account_payment_order_return>=15.0dev,<15.1dev',
         'odoo-addon-account_payment_partner>=15.0dev,<15.1dev',
-        'odoo-addon-account_payment_purchase>=15.0dev,<15.1dev',
-        'odoo-addon-account_payment_purchase_stock>=15.0dev,<15.1dev',
         'odoo-addon-account_payment_sale>=15.0dev,<15.1dev',
     ],
     classifiers=[
